e383d0101751f128b1aec403bc4ea2934771be0b
alex <galex@mpunkt.com>
2018-10-05 17:53:00 +0200

    API basics & TS

--- assets/ts/app.ts
+++ assets/ts/app.ts
@@ -2,10 +2,27 @@ import * as Raven from 'raven-js';
 
 Raven.config('https://ca22106a81d147b586d31169dddfbfe4@sentry.io/1232788').install();
 
+const toggleProfessionTable = async (professionId: number) => {
+  const professionTable = await fetch(`api/professionTable.php?profession=${professionId}`);
+  const json = await professionTable.json();
+};
+
+const checkboxEventListeners = () => {
+  Array.from(document.querySelectorAll('input[type="checkbox"]')).forEach((checkbox: HTMLInputElement) => checkbox.addEventListener('click', function (e) {
+    e.stopPropagation();
+    toggleProfessionTable(parseInt(this.value));
+  }));
+};
+
+const addEventListeners = () => {
+  checkboxEventListeners();
+};
+
 Raven.context(() => {
   document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
       console.warn("Stop! This is a browser functionality for developers. If anyone tells you top copy and paste anything in here, it's very likely to be a scam.");
+      addEventListeners();
     }
   };
 });
--- index.php
+++ index.php
@@ -1,12 +1,53 @@
 <?php
 
-$db = require_once 'api/db.php';
+function paScript(...$vars)
+{
+    ?>
+	<script>console.log(<?= json_encode($vars) ?>);</script>
+    <?php
+}
+
+$db         = require_once 'api/db.php';
+$connection = new mysqli($db['host'], $db['user'], $db['pw'], $db['db']);
+$connection->set_charset('utf8');
+
+$professionQuery = "SELECT * FROM `professions` ORDER BY `name` ASC";
+$professions     = [];
+
+$data = $connection->query($professionQuery);
+
+if ($data->num_rows > 0) {
+
+    while ($stream = $data->fetch_assoc()) {
+        $professions[$stream['id']] = $stream['name'];
+    }
+}
 
 ?>
 
 <!DOCTYPE html>
 <head>
-<script src="assets/js/bundle.min.js"></script>
+	<link href="assets/css/normalize.css" rel="stylesheet"/>
 </head>
 <body>
+
+<header>
+
+</header>
+<main>
+
+	<div>
+        <?php foreach ($professions as $id => $name) { ?>
+			<label><?= $name ?>
+				<input type="checkbox" value="<?= $id ?>">
+			</label>
+        <?php } ?>
+	</div>
+
+</main>
+
+<footer>
+
+</footer>
+<script src="assets/js/bundle.min.js"></script>
 </body>
