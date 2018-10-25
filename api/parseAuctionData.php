53a8c437cd2ae32b6d7749d450a7a84a11998d73
Gerrit Alex <admin@gerritalex.de>
2018-10-25 18:53:17 +0200

    WIP lazy auction data streaming due to server capacity restrictions

--- api/parseAuctionData.php
+++ api/parseAuctionData.php
@@ -11,10 +11,35 @@ $itemIDs = $AuctionCraftSniper->getItemIDs();
 
 $auctionValues = [];
 
-$fileSize  = filesize('blackmoore.json');
+$fileSize = filesize('blackmoore.json');
+
 $byteLimit = 512;
 
-$allowedOverflow = 64;
+function strpos_all($haystack, $needle) {
+    $offset = 0;
+    $allpos = [];
+
+    while (($pos = strpos($haystack, $needle, $offset)) !== false) {
+        $offset   = $pos + 1;
+        $allpos[] = $pos;
+    }
+
+    return $allpos;
+}
+
+function jsLog(...$vars) {
+    echo '<script>console.log(' . json_encode($vars) . ');</script>';
+}
+
+function assignToGlobals(int $occurence, string $key) {
+    global $itemArray;
+    global $previousIndex;
+
+    $itemArray[$key]['value'] = $occurence;
+    $previousIndex            = $key;
+}
+
+
 if ($stream = fopen('blackmoore.json', 'r')) {
 
     ?>
@@ -40,137 +65,62 @@ if ($stream = fopen('blackmoore.json', 'r')) {
                 // check if :$itemID,"owner" exists
                 if (substr_count($data, ':' . $itemID . ',') > 0) {
 
-                    $ownerPosition = strpos($data, $itemID . ',"owner":');
+                    $itemArray = [
+                        'ownerPosition'      => ['position' => 0, 'value' => 0, 'searchString' => $itemID . ',"owner":',],
+                        'ownerRealmPosition' => ['position' => 0, 'value' => 0, 'searchString' => ',"ownerRealm":',],
+                        'bidPosition'        => ['position' => 0, 'value' => 0, 'searchString' => ',"bid":',],
+                        'buyoutPosition'     => ['position' => 0, 'value' => 0, 'searchString' => ',"buyout":',],
+                        'quantityPosition'   => ['position' => 0, 'value' => 0, 'searchString' => ',"quantity":',],
+                        'timeLeftPosition'   => ['position' => 0, 'value' => 0, 'searchString' => ',"timeLeft":',],
+                    ];
 
-                    if (!$ownerPosition) {
-                        $data          .= stream_get_contents($stream, $allowedOverflow, $bytes + $byteLimit);
-                        $ownerPosition = strpos($data, '"item":' . $itemID . ',"owner":');
+                    $previousIndex   = '';
+                    $allowedOverflow = 32;
+
+                    foreach ($itemArray as $key => $info) {
+                        $occurences = strpos_all($data, $info['searchString']);
+
+                        foreach ($occurences as $occurence) {
+                            if ($occurence > $itemArray[$previousIndex]['position']) {
+                                $itemArray[$key]['position'] = $occurence;
+                                $previousIndex               = $key;
+                                break;
+                            }
+                        }
+
+                        // no valid occurence could be found, append more data
+                        if ($itemArray[$key]['position'] === 0) {
+                            $data            .= stream_get_contents($stream, $allowedOverflow, $bytes + $byteLimit);
+                            $allowedOverflow += $allowedOverflow;
+                            $occurences      = strpos_all($data, $info['searchString']);
+
+                            foreach ($occurences as $occurence) {
+                                if ($occurence > $itemArray[$previousIndex]['position']) {
+                                    $itemArray[$key]['position'] = $occurence;
+                                    $previousIndex               = $key;
+                                    break;
+                                }
+                            }
+                        }
                     }
 
-                    $ownerRealmPosition = strpos($data, ',"ownerRealm":');
+                    if ($itemArray['ownerPosition']['position'] < $itemArray['ownerRealmPosition']['position'] && $itemArray['ownerRealmPosition']['position'] < $itemArray['bidPosition']['position'] && $itemArray['bidPosition']['position'] < $itemArray['buyoutPosition']['position'] && $itemArray['buyoutPosition']['position'] < $itemArray['quantityPosition']['position'] && $itemArray['quantityPosition']['position'] < $itemArray['timeLeftPosition']['position']) {
 
-                    if (!$ownerRealmPosition) {
-                        $data               .= stream_get_contents($stream, $allowedOverflow, $bytes + $byteLimit);
-                        $ownerRealmPosition = strpos($data, ',"ownerRealm":');
-                    }
-
-                    $bidPosition = strpos($data, ',"bid":');
-
-                    if (!$bidPosition) {
-                        $data        .= stream_get_contents($stream, $allowedOverflow, $bytes + $byteLimit);
-                        $bidPosition = strpos($data, ',"bid":');
-                    }
-
-                    $buyoutPosition = strpos($data, ',"buyout":');
-
-                    if (!$buyoutPosition) {
-                        $data           .= stream_get_contents($stream, $allowedOverflow, $bytes + $byteLimit);
-                        $buyoutPosition = strpos($data, ',"buyout":');
-                    }
-
-                    $quantityPosition = strpos($data, ',"quantity":');
-
-                    if (!$quantityPosition) {
-                        $data             .= stream_get_contents($stream, $allowedOverflow, $bytes + $byteLimit);
-                        $quantityPosition = strpos($data, ',"quantity":');
-                    }
-
-                    $timeLeftPosition = strpos($data, ',"timeLeft":');
-
-                    if (!$timeLeftPosition) {
-                        $data             .= stream_get_contents($stream, $allowedOverflow, $bytes + $byteLimit);
-                        $timeLeftPosition = strpos($data, ',"timeLeft":');
-                    }
-
-
-                    /*if (!$ownerPosition || !$ownerRealmPosition || !$bidPosition || !$buyoutPosition || !$quantityPosition || !$timeLeftPosition) {
-                        echo $itemID . ' => ' . $data;
+                    } else {
+                        jsLog($itemArray, $data);
                         die;
-                    }*/
-
+                    }
 
                     ?>
 					<tr>
 						<td><?= $itemID ?></td>
-						<td><?= $ownerPosition ?></td>
-						<td><?= $ownerRealmPosition ?></td>
-						<td><?= $bidPosition ?></td>
-						<td><?= $buyoutPosition ?></td>
-						<td><?= $quantityPosition ?></td>
-						<td><?= $timeLeftPosition ?></td>
+                        <?php foreach ($itemArray as $key => $info) { ?>
+							<td><?= $info['position'] ?> => <?= $info['value'] ?></td>
+                        <?php } ?>
 					</tr>
                     <?php
 
-                    unset($ownerPosition, $ownerRealmPosition, $bidPosition, $buyoutPosition, $quantityPosition, $timeLeftPosition);
-
-
-                    /*// find position of $itemID
-                    $itemIDPosition = strpos($data, $itemID);
-
-                    if ($itemIDPosition >= 432) {
-                        $data .= stream_get_contents($stream, 32, $bytes + $byteLimit);
-                    }
-
-                    // shorten data
-                    $tempData = substr($data, $itemIDPosition + strlen((string)$itemID));
-
-                    // find next buyout value
-                    $buyoutPosition = strpos($tempData, '"buyout":');
-
-                    if (!$buyoutPosition) {
-                        $tempData       .= stream_get_contents($stream, 96, $bytes + $byteLimit);
-                        $buyoutPosition = strpos($tempData, '"buyout":');
-                    }
-
-                    // shorten data
-                    $tempData = substr($tempData, $buyoutPosition + 9);
-
-                    // find next quantity
-                    $quantityPosition = strpos($tempData, ',"quantity":');
-
-                    if (!$quantityPosition) {
-                        $tempData         .= stream_get_contents($stream, 96, $bytes + $byteLimit);
-                        $quantityPosition = strpos($tempData, ',"quantity":');
-                    }
-
-                    $buyout = substr($tempData, 0, $quantityPosition);
-
-                    // shorten data
-                    $tempData = substr($tempData, $quantityPosition + 12);
-
-                    // find next timeLeft
-                    $timeLeftPosition = strpos($tempData, ',"');
-
-                    if (!$timeLeftPosition) {
-                        $tempData         .= stream_get_contents($stream, 96, $bytes + $byteLimit);
-                        $timeLeftPosition = strpos($tempData, ',"');
-                    }
-
-                    $quantity = substr($tempData, 0, $timeLeftPosition);
-
-
-                    $thisPrice     = round((int)$buyout / (int)$quantity);
-                    $previousPrice = (int)$auctionValues[$itemID];
-
-                    if ($thisPrice < $previousPrice || $previousPrice === 0) {
-                        $auctionValues[$itemID] = $thisPrice;
-                    }
-
-                    if ((int)$buyout === 0 || (int)$quantity === 0 || (int)$thisPrice === 0) { ?>
-						<tr>
-							<td><?= $itemID ?></td>
-							<td><?= $buyout ?></td>
-							<td><?= $quantity ?></td>
-							<td><?= $thisPrice ?></td>
-							<td>
-								<pre><?= $tempData ?></pre>
-							</td>
-						</tr>
-                    <?php }
-
-                    #echo '<pre>thisPrice = ' . $thisPrice . ' // itemID = ' . $itemID . ' @ ' . $position . ' // buyout @ ' . $buyoutPosition . ' // quantity @ ' . $quantityPosition . ' // buyout = ' . $buyout . ' // quantity = ' . $quantity . '</pre>' . $data . ' <hr >';
-
-                    unset($position, $buyoutPosition, $quantityPosition, $buyout, $timeLeftPosition, $quantity, $thisPrice, $previousPrice, $tempData);*/
+                    $auctionValues[] = $itemArray;
                 }
             }
         }
