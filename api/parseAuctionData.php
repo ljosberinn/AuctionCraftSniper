809c7626bcfdb7a3fb34b81901155596e5ba24af
Gerrit Alex <admin@gerritalex.de>
2018-10-26 17:28:27 +0200

    WIP lazy auction data streaming due to server capacity restrictions

--- api/parseAuctionData.php
+++ api/parseAuctionData.php
@@ -1,49 +1,83 @@
 <?php
 
-ini_set('max_execution_time', 60);
-
-require_once '../dependencies/headers.php';
-require_once '../dependencies/class.AuctionCraftSniper.php';
-
-$AuctionCraftSniper = new AuctionCraftSniper(false, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
-$itemIDs = $AuctionCraftSniper->getItemIDs();
-
-
 function jsLog(...$vars) {
     echo '<script>console.log(' . json_encode($vars) . ');</script>';
 }
 
+require_once '../dependencies/headers.php';
 
-$auctionValues    = [];
-$fileSize         = filesize('blackmoore.json');
-$byteLimit        = 365;
+const CHUNK_SIZE         = 2700000;
+const AUCTION_END_STRING = ',{"auc"';
+const BYTE_LIMIT         = 365;
 
-$auctionEndString = ',{"auc"';
-$leftovers        = '';
+if (!isset($_POST['step']) && !isset($_POST['auctionValues']) && !isset($_POST['itemIDs'])) {
 
-if ($stream = fopen('blackmoore.json', 'r')) {
+    require_once '../dependencies/class.AuctionCraftSniper.php';
 
-    $first200Bytes = stream_get_contents($stream, 200, 0);
-    $auctionsStart = strpos($first200Bytes, '"auctions": [') + 16;
+    $AuctionCraftSniper = new AuctionCraftSniper(false, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
-    for ($bytes = $auctionsStart; $bytes <= 4000000; $bytes += $byteLimit) {
+    $itemIDs = $auctionValues = [];
+
+    foreach ($AuctionCraftSniper->getItemIDs() as $itemID) {
+        $itemIDs[$itemID] = true;
+    }
+
+    $step = 1;
+} else {
+    $auctionValues = (array)json_decode($_POST['auctionValues']);
+    $itemIDs       = (array)json_decode($_POST['itemIDs']);
+    $step          = (int)$_POST['step'];
+
+    if ($step <= 1) {
+        echo json_encode(['err' => 'invalid step specified']);
+        die;
+    }
+}
+
+$fileName = (int)$_POST['houseID'];
+#$fileName = 'blackmoore.json';
+
+if ($fileName === 0) {
+    echo json_encode(['err' => 'invalid house specified']);
+    die;
+}
+
+if ($stream = fopen($fileName, 'r')) {
+
+    if (!isset($_POST['step'])) {
+        $first200Bytes = stream_get_contents($stream, 200, 0);
+        $auctionsStart = strpos($first200Bytes, '"auctions": [') + 16;
+
+        $nextChunkEnd = CHUNK_SIZE;
+    } else {
+        $nextChunkEnd  = CHUNK_SIZE * $step;
+        $auctionsStart = $nextChunkEnd - CHUNK_SIZE;
+    }
+
+    $fileSize  = filesize($fileName);
+    $leftovers = '';
+
+    if ($nextChunkEnd > $fileSize) {
+        $nextChunkEnd = $fileSize;
+    }
+
+    for ($bytes = $auctionsStart; $bytes <= $nextChunkEnd; $bytes += BYTE_LIMIT) {
 
         // get trailing auction data from previous iteration to have a full new dataset
         $data = $leftovers;
 
         // remove whitespace & linebreaks
-        $data .= str_replace("	", '', str_replace("\r\n", '', stream_get_contents($stream, $byteLimit, $bytes)));
+        $data .= str_replace("	", '', str_replace("\r\n", '', stream_get_contents($stream, BYTE_LIMIT, $bytes)));
 
         // find end of this auction
-        $auctionEnd = strpos($data, $auctionEndString);
+        $auctionEnd = strpos($data, AUCTION_END_STRING);
 
         // define new leftovers for next iteration, +1 because of leading , at start of new auction obj
         $leftovers = substr($data, $auctionEnd + 1);
 
         $data = json_decode(substr($data, 0, $auctionEnd), true);
 
-        if (in_array($data['item'], $itemIDs)) {
+        if (isset($itemIDs[$data['item']])) {
             $thisPPU     = round($data['buyout'] / $data['quantity']);
             $previousPPU = (int)$auctionValues[$data['item']];
 
@@ -55,5 +89,11 @@ if ($stream = fopen('blackmoore.json', 'r')) {
 
     fclose($stream);
 
-    echo json_encode($auctionValues);
+    $response = [
+        'auctionValues' => $auctionValues,
+        'itemIDs'       => $itemIDs,
+        'step'          => $nextChunkEnd === $fileSize ? -1 : $step + 1,
+    ];
+
+    echo json_encode($response);
 }
