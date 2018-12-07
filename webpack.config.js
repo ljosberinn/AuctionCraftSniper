8bbdb027dfd837a748b60987d07c2d51a83eddca
Gerrit Alex <admin@gerritalex.de>
2018-12-07 16:42:56 +0100

    added polyfill for datalist

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './assets/js/app.js',
+  entry: ['./assets/js/app.js', './node_modules/datalist-polyfill/datalist-polyfill.min.js'],
   output: {
     path: path.resolve(__dirname, './assets/js'),
     filename: 'bundle.min.js',
