086473125df3b7a41285080d04be75badbedbaa9
ljosberinn <admin@gerritalex.de>
2018-12-13 00:30:12 +0100

    WIP - transpiling TS down to ES3, avoiding several user errors with fetch, Object.entries() and document.querySelectorAll().forEach being not a function or property

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: ['./assets/js/app.js', './node_modules/datalist-polyfill/datalist-polyfill.min.js'],
   output: {
     path: path.resolve(__dirname, './assets/js'),
     filename: 'bundle.min.js',
+  },
+  devtool: 'hidden-source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+    ],
   },
 };
